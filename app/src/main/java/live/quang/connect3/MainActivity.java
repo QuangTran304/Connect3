5e41eba394e72e51ec83a6c969ae0ef408e04c5f
Quang Tran <nhatquang304@gmail.com>
2020-01-01 23:02:37 +0700

    fix error: stop player from replacing the other's position.

--- app/src/main/java/live/quang/connect3/MainActivity.java
+++ app/src/main/java/live/quang/connect3/MainActivity.java
@@ -16,32 +16,36 @@ public class MainActivity extends AppCompatActivity {
 
 
     public void tapOn( View view ) {
-        ImageView anImage = (ImageView) view;           // "view" object from parameter
-        int tappedImage = Integer.parseInt( anImage.getTag().toString() );  // get the location of the tapped image.
-        gameBoard[tappedImage] = activePlayer;          // Located the player on game board, according to the tapped place.
+        ImageView anImage = (ImageView) view;               // "view" object from parameter
+        int tappedImage = Integer.parseInt( anImage.getTag().toString() );  // get the location/the tag of the tapped image.
 
-        if ( activePlayer == 0 ) {
-            anImage.setImageResource( R.drawable.o );   // set an image of "o", change active player
-            activePlayer = 1;
-        } else {
-            anImage.setImageResource( R.drawable.x );   // set an image of "x", change active player
-            activePlayer = 0;
-        }
+        if (gameBoard[tappedImage] == 9) {                  // If the place is empty ( == 9), allow player to continue. If it is already reserved for a player 'x' or 'o', then do nothing.
+
+            gameBoard[tappedImage] = activePlayer;          // Located the player on game board, according to the tapped place.
+
+            if ( activePlayer == 0 ) {
+                anImage.setImageResource( R.drawable.o );   // set an image of "o", change active player
+                activePlayer = 1;
+            } else {
+                anImage.setImageResource( R.drawable.x );   // set an image of "x", change active player
+                activePlayer = 0;
+            }
 
 
-        // We need to loop through all the winning positions to find out if they all got the same values (either all '0' or all '1')
-        // AND they are not == to 9 (empty place)
-        for ( int[] winningPos : winningPositions ) {
-            if ( gameBoard[winningPos[0]] == gameBoard[winningPos[1]]  &&  gameBoard[winningPos[1]] == gameBoard[winningPos[2]]  &&  gameBoard[winningPos[0]] != 9 ) {     // We only need to check for one position != 9
+            // We need to loop through all the winning positions to find out if they all got the same values (either all '0' or all '1')
+            // AND they are not == to 9 (empty place)
+            for ( int[] winningPos : winningPositions ) {
+                if ( gameBoard[winningPos[0]] == gameBoard[winningPos[1]]  &&  gameBoard[winningPos[1]] == gameBoard[winningPos[2]]  &&  gameBoard[winningPos[0]] != 9 ) {     // We only need to check for one position != 9
 
-                // Someone has won. Check who it exactly is:
-                String winner = "";
-                if ( activePlayer == 1 ) {
-                    winner = "Player O";        // Because we already change to active player after one of them click, so it is reversed.
-                } else {
-                    winner = "Player X";
+                    // Someone has won. Check who it exactly is:
+                    String winner = "";
+                    if ( activePlayer == 1 ) {
+                        winner = "Player O";        // Because we already change to active player after one of them click, so it is reversed.
+                    } else {
+                        winner = "Player X";
+                    }
+                    Toast.makeText(this, winner + " has won!!!", Toast.LENGTH_LONG).show();
                 }
-                Toast.makeText(this, winner + " has won!!!", Toast.LENGTH_LONG).show();
             }
         }
 
